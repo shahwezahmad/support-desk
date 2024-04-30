41183395ffcfb42fc581d40b1b3765dab7b4ed8d
Shahwez Ahmad <shahwez@Shahwezs-MacBook-Air.local>
2024-04-30 22:57:18 +0530

    setup eexpress

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('Server started..');
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT
+const app = express()
+
+app.get('/', (req,res)=> {
+    res.send('from / route')
+})
+
+app.get('/new/route', (req,res)=> {
+    res.status(202).json({message:"from new route"})
+})
+
+app.listen(PORT, ()=> console.log(`Server started in ${PORT}`))
